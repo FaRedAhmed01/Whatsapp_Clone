f0be1144e151b9cf8e15758269eccaa81c167b86
Fared Ahmed <faredahmed01@gmail.com>
2019-12-16 18:09:08 +0200

    first commit

---
+++ lib/models/chat_model.dart
@@ -0,0 +1,47 @@
+class ChatModel {
+  final String name;
+  final String message;
+  final String time;
+  final String avatarUrl;
+
+  ChatModel({this.name, this.message, this.time, this.avatarUrl});
+}
+
+List<ChatModel> dummyData = [
+  new ChatModel(
+      name: "Fared Ahmed",
+      message: "Hey Flutter, You are so amazing !",
+      time: "15:30",
+      avatarUrl:
+          "http://www.usanetwork.com/sites/usanetwork/files/styles/629x720/public/suits_cast_harvey.jpg?itok=fpTOeeBb"),
+  new ChatModel(
+      name: "Fared Ahmed",
+      message: "Hey I have hacked whatsapp!",
+      time: "17:30",
+      avatarUrl:
+          "http://www.usanetwork.com/sites/usanetwork/files/styles/629x720/public/suits_cast_harvey.jpg?itok=fpTOeeBb"),
+  new ChatModel(
+      name: "Fared Ahmed",
+      message: "Wassup !",
+      time: "5:00",
+      avatarUrl:
+          "http://www.usanetwork.com/sites/usanetwork/files/styles/629x720/public/suits_cast_harvey.jpg?itok=fpTOeeBb"),
+  new ChatModel(
+      name: "Fared Ahmed",
+      message: "I'm good!",
+      time: "10:30",
+      avatarUrl:
+          "http://www.usanetwork.com/sites/usanetwork/files/styles/629x720/public/suits_cast_harvey.jpg?itok=fpTOeeBb"),
+  new ChatModel(
+      name: "Fared Ahmed",
+      message: "I'm the fastest man alive!",
+      time: "12:30",
+      avatarUrl:
+          "http://www.usanetwork.com/sites/usanetwork/files/styles/629x720/public/suits_cast_harvey.jpg?itok=fpTOeeBb"),
+  new ChatModel(
+      name: "Fared Ahmed",
+      message: "Hey Flutter, You are so cool !",
+      time: "15:30",
+      avatarUrl:
+          "http://www.usanetwork.com/sites/usanetwork/files/styles/629x720/public/suits_cast_harvey.jpg?itok=fpTOeeBb"),
+];
